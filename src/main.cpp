558659901742ab4f7de5eeeed0f7354fa9e49861
beau28713 <beau28713@gmail.com>
2025-06-05 22:40:47 -0400

    Added code for sending floating point data to ignition via input registers.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
 const int relay_pin = LED_BUILTIN;
+const int potentiometer_pin = A0;
 int status = WL_IDLE_STATUS;
 
 WiFiServer server(502);
@@ -16,6 +17,21 @@ ModbusTCPServer modbusTCPServer;
 void updaterelay() {
   bool state = modbusTCPServer.coilRead(0x00);
   digitalWrite(relay_pin, state ? HIGH : LOW);
+}
+
+void updatepotentiometer() {
+  int potValue = analogRead(potentiometer_pin);
+  float potVoltage = potValue *(5.0/1024.0);
+
+  uint32_t dualValue1 = *((uint32_t *)&potVoltage); // Convert float to uint32_t for Modbus
+  uint16_t register1 = dualValue1 >> 16;  // right shift >> by 16bits
+  uint16_t register2 = dualValue1 & 0xffff; // mask to get the lower 16 bits
+
+  int potResistence = map(potValue, 0, 1023, 0, 10000);
+  delay(10); // Allow for analog read settling
+  modbusTCPServer.inputRegisterWrite(0x02, potResistence); // Write potentiometer value to input register
+  modbusTCPServer.inputRegisterWrite(0x00, register1); // Write potentiometer voltage to input register
+  modbusTCPServer.inputRegisterWrite(0x01, register2); // Write potentiometer voltage to input register
 }
 
 void setup() {
@@ -45,7 +61,14 @@ void setup() {
 
   modbusTCPServer.configureCoils(0x00, 5);  // Configure 5 coils starting at address 0x00 Modbus address default starts at 1 in ignition. 
                                             // you must go into advance setting in ignition to set the starting address to 0 by checkng the box in device connections in your device you are using.
+
+  modbusTCPServer.configureInputRegisters(0x00, 5); // Configure 5 input registers starting at address 0x00
+
+  
   pinMode(relay_pin, OUTPUT);
+  pinMode(potentiometer_pin, INPUT);
+
+  // Initialize relay to LOW (off)
   digitalWrite(relay_pin, LOW);
 
   Serial.println("Modbus server started.");
@@ -61,6 +84,7 @@ void loop() {
     while (client.connected()) {
       modbusTCPServer.poll();        // Handle Modbus request
       updaterelay();                 // Sync output to coil state
+      updatepotentiometer();         // Sync input register to potentiometer value
       delay(10);                     // Allow brief socket breathing room
     }
 
